4f8bc8c58f56eaf57416221a430fa44f226006e3
PokeStar04 <romain.monfret@hetic.net>
2022-03-29 03:53:50 +0200

    Update navigation.php

--- _nav/navigation.php
+++ _nav/navigation.php
@@ -1,5 +1,20 @@
 <nav>
-<li><a href="/">acceuil</a></li>
-<li><a href="../connexion.php">Connexion</a></li>   
-<li><a href="../inscription.php">Incription</a></li>
+    <li><a href="/">acceuil</a></li>
+    <li><a href="../voirAnnonce.php">Voir annonce</a></li>
+    <?php
+    if(!isset($_SESSION['id'])){
+    ?>
+        <li><a href="../connexion.php">Connexion</a></li>
+        <li><a href="../inscription.php">Incription</a></li>
+    <?php
+    }else{
+    ?>
+        <li><a href="../deconnexion.php">Deconnexion</a></li>
+        <li><a href="../annonce.php">Crée une annonce</a></li>
+    <?php
+        }
+    ?>
+
+
+
 </nav>
