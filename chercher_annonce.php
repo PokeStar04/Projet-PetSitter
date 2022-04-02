3d75feee04efc266627b8fa7188c4d367173857c
91Lucas19 <lucas.compiano@hetic.net>
2022-04-02 23:39:25 +0200

    chercher_annonce.php
    cassé mais c pour romain

--- chercher_annonce.php
+++ chercher_annonce.php
@@ -13,17 +13,6 @@ $var = 'Bonjour à tous';
 }
 
 
-
-
-
-
-
-
-$pays = htmlspecialchars(trim($_POST['pays']));
-$postal = htmlspecialchars(trim($_POST['postal']));
-
-
-
 ?>
 
 
@@ -51,9 +40,9 @@ require_once('./_head/script.php');
     
 <!-- utilisateur -->
 <label>Pays</label>
-<input type="text" name="pays" placeholder="pays" value="<?php if (isset($pays)) echo $pays; ?>">
+<input type="text" name="pays" placeholder="pays" >
 <label>Code postal</label>
-<input type="text" name="postal" placeholder="postal" value="<?php if (isset($postal)) echo $postal; ?>"></br>
+<input type="text" name="postal" placeholder="postal" ></br>
 
 
 <!-- annonce -->
@@ -72,24 +61,24 @@ require_once('./_head/script.php');
 </br>
 
 <!-- animaux -->
-<!-- <label>Chien</label>
-<input type="checkbox" name="chien" placeholder="chien" value="chien">
+<label>Chien</label>
+<input type="checkbox" name="animaux" placeholder="chien" value="chien">
 <label>Chat</label>
-<input type="checkbox" name="chat" placeholder="chat" value="chat">
+<input type="checkbox" name="animaux" placeholder="chat" value="chat">
 <label>Lapin</label>
-<input type="checkbox" name="lapin" placeholder="lapin" value="lapin"></br>
+<input type="checkbox" name="animaux" placeholder="lapin" value="lapin"></br>
 <label>Rongeur</label>
-<input type="checkbox" name="rongeur" placeholder="rongeur" value="rongeur">
+<input type="checkbox" name="animaux" placeholder="rongeur" value="rongeur">
 <label>Furet</label>
-<input type="checkbox" name="furet" placeholder="furet" value="furet">
+<input type="checkbox" name="animaux" placeholder="furet" value="furet">
 <label>Hérisson</label>
-<input type="checkbox" name="herisson" placeholder="herisson" value="herisson"></br>
+<input type="checkbox" name="animaux" placeholder="herisson" value="herisson"></br>
 <label>Aquarium</label>
-<input type="checkbox" name="aquarium" placeholder="aquarium" value="aquarium">
+<input type="checkbox" name="animaux" placeholder="aquarium" value="aquarium">
 <label>Oiseaux</label>
-<input type="checkbox" name="oiseaux" placeholder="oiseaux" value="oiseaux">
+<input type="checkbox" name="animaux" placeholder="oiseaux" value="oiseaux">
 <label>Reptiles</label>
-<input type="checkbox" name="reptiles" placeholder="reptiles" value="reptiles"></br> -->
+<input type="checkbox" name="animaux" placeholder="reptiles" value="reptiles"></br>
 
 <input type="submit">
 </form>
@@ -97,58 +86,39 @@ require_once('./_head/script.php');
 </br>
 </br>
 <?php
+
+
 //utilisateur
 if (isset($_POST['pays']) && (!empty($_POST['pays'])) && isset($_POST['postal']) && (!empty($_POST['postal']))){
 
     ?><a></br></a><?php
-
-    // $preparedRequest = $DB->prepare('INSERT INTO search_annonce (pays2, ville2 ,startDate2 ,endDate2) VALUES (:pays2,:ville2,:startDate2,:endDate2)');
-
-    // $preparedRequest->bindValue('pays2', $_POST['pays2'], PDO::PARAM_STR);
-    // $preparedRequest->bindValue('ville2', $_POST['prenom'], PDO::PARAM_STR);
-    // $preparedRequest->bindValue('starDate2', $_POST['startDate2'], PDO::PARAM_INT);
-    // $preparedRequest->bindValue('endDate2', $_POST['endDate2'], PDO::PARAM_STR);
-    // $preparedRequest->execute();
-
 
 
     $sql = "SELECT pays, postal FROM `utilisateur`";
     $query = $DB->prepare($sql);
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
     foreach($result as $geo){
 
-        $annonce = "SELECT * FROM `annonce` WHERE userID IN (SELECT id FROM `utilisateur` WHERE pays =".$pays.")";
-        $query->bindValue('pays', $_POST['pays'], PDO::PARAM_STR);
+        $annonce = "SELECT * FROM `annonce` WHERE userID IN (SELECT id FROM `utilisateur` WHERE pays,  LIKE '%" .$_POST['pays']."%'  )";
+    
+        $query = $DB->prepare($annonce);
+        $query->execute();
+        $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    }  
+    var_dump ($result);
+    foreach($result as $geo){
+
+        $annonce = "SELECT * FROM `annonce` WHERE userID IN (SELECT id FROM `utilisateur` WHERE postal LIKE '%" .$_POST['postal']."%')";
+    
         $query = $DB->prepare($annonce);
         $query->execute();
         $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
 
 
-        // if ($_POST('pays') == $PAYS)
     }
     var_dump ($result);
-
-    // foreach($result as $geo){
-
-    //     $annonce = "SELECT * FROM `annonce` WHERE userID IN (SELECT id FROM `utilisateur` WHERE".$postal.  ")";
-    //     $query->bindValue('pays', $_POST['postal'], PDO::PARAM_STR);
-    //     $query = $DB->prepare($annonce);
-    //     $query->execute();
-    //     $result = $query->fetchAll(PDO::FETCH_ASSOC);
-
-
-
-    //     // if ($_POST('pays') == $PAYS)
-    // }
-    // var_dump ($result);
-
-
-
-
-
  }
 else{
     echo ("mettre les informations");
@@ -210,49 +180,92 @@ else{
 
 //animaux
 
-// if (!empty($_POST)) {
 
-//     if (!empty($_POST['chien'])){
-//         echo $_POST['chien'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['chat'])){
-//         echo $_POST['chat'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['lapin'])){
-//         echo $_POST['lapin'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['rongeur'])){
-//         echo $_POST['rongeur'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['furet'])){
-//         echo $_POST['furet'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['herisson'])){
-//         echo $_POST['herisson'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['aquarium'])){
-//         echo $_POST['aquarium'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['oiseaux'])){
-//         echo $_POST['oiseaux'];
-//         ?><a></br></a><?php
-//     }
-//     if (!empty($_POST['reptiles'])){
-//         echo $_POST['reptiles'];
-//         ?><a></br></a><?php
-//     }
-// }
-// else{
-//     ?>  
-<!-- //     <a>choisissez un animal</a> -->    <?php
-// }
+
+
+if (!empty($_POST)) {
+    $qq = $DB->prepare('SELECT * FROM animaux ');
+    $qq->execute();
+    $animAll = $qq->fetchAll(PDO::FETCH_ASSOC);
+    $sql = "SELECT * FROM `animaux` WHERE ".$_POST['animaux']."=1";
+    $query = $DB->prepare($sql);
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    $AZER = array_intersect($animAll , $result);
+    print_r($AZER);
+
+
+
+
+    $result
+    $bool = true;
+
+
+
+
+    if (($_POST['animaux'])== 'chien'){
+        ?><a></br></a><?php
+        foreach($result as $geo){
+
+            $annonce = 'SELECT * FROM `annonce` WHERE userID IN (SELECT userID FROM animaux WHERE `chien`="1")';
+        
+            $query = $DB->prepare($annonce);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+    
+    
+    }
+
+    if (($_POST['animaux'])== 'chat'){
+        ?><a></br></a><?php
+        foreach($result as $geo){
+
+            $annonce = 'SELECT * FROM `annonce` WHERE userID IN (SELECT userID FROM animaux WHERE `chat`="1")';
+        
+            $query = $DB->prepare($annonce);
+            $query->execute();
+            $result = $query->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    
+    }
+
+
+    if (!empty($_POST['lapin'])){
+        echo $_POST['lapin'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['rongeur'])){
+        echo $_POST['rongeur'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['furet'])){
+        echo $_POST['furet'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['herisson'])){
+        echo $_POST['herisson'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['aquarium'])){
+        echo $_POST['aquarium'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['oiseaux'])){
+        echo $_POST['oiseaux'];
+        ?><a></br></a><?php
+    }
+    if (!empty($_POST['reptiles'])){
+        echo $_POST['reptiles'];
+        ?><a></br></a><?php
+    }
+}
+else{
+     ?>  
+     <a>choisissez un animal</a>    <?php
+ }
 
 ?>
 
